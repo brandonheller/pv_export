b507eaaa6807a30ce1298f29e8c87d6e95b01a7a
Brandon Heller <brandonh@stanford.edu>
2011-02-07 00:48:09 -0800

    Make pv2svg less verbose

--- pv2svg.js
+++ pv2svg.js
@@ -2,23 +2,23 @@
 
 /** 
  * Input args:
- *  visfile: JS code to render protovis, should use a 'data' var
- *  datafile: JS code, should populate a 'data' var
- *  outfile: SVG output file path, as URI
- *  htmlfile: HTML page to load first w/'mycanvas' div defined, as URI
+ *  vis: JS code to render protovis, should use a 'data' var
+ *  data: JS code, should populate a 'data' var
+ *  out: SVG output file path, as URI
+ *  html: HTML page to load first w/'mycanvas' div defined, as URI
  */
-var visfile = arguments[0];
-var datafile = arguments[1];
-var outfile = arguments[2];
-var htmlfile = arguments[3];
+var vis = arguments[0];
+var data = arguments[1];
+var out = arguments[2];
+var html = arguments[3];
 
 load('env.rhino.js');
 
-Envjs(htmlfile);
+Envjs(html);
 Envjs.wait(); // ensure DOM has finished loading
 
 load('protovis-d3.2.js');
-load(datafile);
-load(visfile);
+load(data);
+load(vis);
 
-Envjs.writeToFile(document.getElementById('mycanvas').innerHTML, outfile);
+Envjs.writeToFile(document.getElementById('mycanvas').innerHTML, out);
